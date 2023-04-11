c15bd945112a8ae7482aa1079d53e26f16a09cf3
Ian Tapply <tapplyi@students.dsbn.org>
2023-04-11 12:42:46 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = '1114 Scouting Platform'
+copyright = '2023, Team 1114 Simbotics'
+author = 'Ian Tapply'
 
 release = '0.1'
 version = '0.1.0'
